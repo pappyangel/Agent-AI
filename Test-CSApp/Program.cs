901421ef754becdb343a1e4cd67a32ebd0da0186
Jim Vacca <33520581+MSFTJim@users.noreply.github.com>
2026-07-26 00:10:19 +0000

    add new config logic into  Test App

--- Test-CSApp/Program.cs
+++ Test-CSApp/Program.cs
@@ -1,22 +1,32 @@
 ﻿using System.Text.Json;
 using Microsoft.Extensions.Configuration;
 
-Console.WriteLine("Start of Program");
+Console.WriteLine("--- Start of Program ---");
+ 
+// Load configuration from appsettings.json, user secrets, and environment variables
+string subName = string.Empty;
+string tenantId = string.Empty;
+string contentSafetyEndpoint = string.Empty;
+string projectEndpoint = string.Empty;
+string modelDeploymentName = string.Empty;
+string agentName = string.Empty;
 
-var (projectEndpoint, modelDeploymentName, contentsafetyEndpoint, agentName) = LoadConfiguration();
-
+LoadConfiguration();
 GetEnvVars();
 
-Console.WriteLine("End of Program");
+Console.WriteLine($"Project Endpoint from Config: {subName}");
+
+Console.WriteLine("--- End of Program ---");
 //////// MAIN Program END ////////
 
 void GetEnvVars()
 {
-    string projectEndpoint = Environment.GetEnvironmentVariable("PROJECT_ENDPOINT") ?? "<not set>";
-    string modelDeploymentName = Environment.GetEnvironmentVariable("MODEL_DEPLOYMENT_NAME") ?? "<not set>";
-    string contentSafetyEndpoint = Environment.GetEnvironmentVariable("CSAFE_ENDPOINT") ?? "<not set>";
-    string tenantId = Environment.GetEnvironmentVariable("TENANT_ID") ?? "<not set>";
-    string subName = Environment.GetEnvironmentVariable("SUB_NAME") ?? "<not set>";
+    projectEndpoint = Environment.GetEnvironmentVariable("PROJECT_ENDPOINT") ?? "<not set>";
+    modelDeploymentName = Environment.GetEnvironmentVariable("MODEL_DEPLOYMENT_NAME") ?? "<not set>";
+    contentSafetyEndpoint = Environment.GetEnvironmentVariable("CSAFE_ENDPOINT") ?? "<not set>";
+    tenantId = Environment.GetEnvironmentVariable("TENANT_ID") ?? "<not set>";
+    subName = Environment.GetEnvironmentVariable("SUB_NAME") ?? "<not set>";
+    
 
     Console.WriteLine($"Project Endpoint: {projectEndpoint}");
     Console.WriteLine($"Model Deployment Name: {modelDeploymentName}");
@@ -25,8 +35,7 @@ void GetEnvVars()
     Console.WriteLine($"Subscription Name: {subName}");
 }
 
-// Load configuration from appsettings, user secrets, and environment variables
-(string, string, string, string) LoadConfiguration()
+ void LoadConfiguration()
 {
     var config = new ConfigurationBuilder()
         .SetBasePath(Directory.GetCurrentDirectory())
@@ -35,17 +44,11 @@ void GetEnvVars()
         .AddEnvironmentVariables()
         .Build();
 
-    string projectEndpointFromConfig = config["PROJECT_ENDPOINT"]
-        ?? throw new InvalidOperationException("Missing PROJECT_ENDPOINT");
 
-    string modelDeploymentNameFromConfig = config["MODEL_DEPLOYMENT_NAME"]
-        ?? throw new InvalidOperationException("Missing MODEL_DEPLOYMENT_NAME");
 
-    string contentsafetyEndpointFromConfig = config["CSAFE_ENDPOINT"]
-        ?? throw new InvalidOperationException("Missing CSAFE_ENDPOINT");
-
-    string agentName = config["AGENT_NAME"]
+    agentName = config["AGENT_NAME"]
         ?? throw new InvalidOperationException("Missing AGENT_NAME");
+    Console.WriteLine($"Agent Name from Config: {agentName}");
 
-    return (projectEndpointFromConfig, modelDeploymentNameFromConfig, contentsafetyEndpointFromConfig, agentName);
+
 }
